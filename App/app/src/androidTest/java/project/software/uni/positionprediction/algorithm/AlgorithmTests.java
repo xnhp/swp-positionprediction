db2681f67364247c3c4cb6869c8e82af18feff94
Sebastian Strumbel <sebastian.strumbelj@uni-konstanz.de>
2018-06-02 14:12:38 +0200

    Add test for arithmetic Location3D methods

--- App/app/src/androidTest/java/project/software/uni/positionprediction/algorithm/AlgorithmTests.java
+++ App/app/src/androidTest/java/project/software/uni/positionprediction/algorithm/AlgorithmTests.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class AlgorithmTests {
 
-    private double delta = 0.000000001;
+    private double delta = 0.000001;
 
     @Test
     public void arithmetic3D() throws Exception {
@@ -29,6 +29,7 @@ public class AlgorithmTests {
         Location3D b = new Location3D(2, -3, 4);
         double number = 3.11;
 
+        // Addition
         Location3D method_add = a.add(b);
         Location3D own_add = new Location3D(6.55, 8, 3);
 
@@ -36,11 +37,29 @@ public class AlgorithmTests {
         assertEquals(method_add.getLoc_lat(), own_add.getLoc_lat(), delta);
         assertEquals(method_add.getLoc_height(), own_add.getLoc_height(), delta);
 
+        // Subtraction
+        Location3D method_sub = a.subtract(b);
+        Location3D own_sub = new Location3D(2.55, 14, -5);
 
+        assertEquals(method_add.getLoc_long(), own_add.getLoc_long(), delta);
+        assertEquals(method_add.getLoc_lat(), own_add.getLoc_lat(), delta);
+        assertEquals(method_add.getLoc_height(), own_add.getLoc_height(), delta);
 
+        // Multiplication
+        Location3D method_mult = a.multiply(number);
+        Location3D own_mult = new Location3D(14.1505, -34.21, -3.11);
 
-        // Todo divide, multiply, subtract
+        assertEquals(method_add.getLoc_long(), own_add.getLoc_long(), delta;
+        assertEquals(method_add.getLoc_lat(), own_add.getLoc_lat(), delta);
+        assertEquals(method_add.getLoc_height(), own_add.getLoc_height(), delta);
 
+        // Division
+        Location3D method_div = a.divide(number);
+        Location3D own_div = new Location3D(1.463022508, -3.536977492, -0.3215434084);
+
+        assertEquals(method_add.getLoc_long(), own_add.getLoc_long(), delta);
+        assertEquals(method_add.getLoc_lat(), own_add.getLoc_lat(), delta);
+        assertEquals(method_add.getLoc_height(), own_add.getLoc_height(), delta);
 
     }
 }
