c4294e929b4c38ff73391a1135a18c447225c48d
Sebastian Strumbel <sebastian.strumbelj@uni-konstanz.de>
2018-06-07 18:53:53 +0200

    Add first changes in menu

--- App/app/src/main/java/project/software/uni/positionprediction/activities/Settings.java
+++ App/app/src/main/java/project/software/uni/positionprediction/activities/Settings.java
@@ -3,8 +3,11 @@ package project.software.uni.positionprediction.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import project.software.uni.positionprediction.R;
 
@@ -15,7 +18,19 @@ public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_settings);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner_alg);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
 
         buttonSave = findViewById(R.id.settings_button_save);
 
