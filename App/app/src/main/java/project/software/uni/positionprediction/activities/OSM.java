f8b7acdd0ac3b0ffa512da4dfc3d7a545de55f2d
Benjamin Moser <benjamin.2.moser@uni-konstanz.de>
2018-06-14 10:51:05 +0200

    renamed variable

--- App/app/src/main/java/project/software/uni/positionprediction/activities/OSM.java
+++ App/app/src/main/java/project/software/uni/positionprediction/activities/OSM.java
@@ -1,13 +1,11 @@
 package project.software.uni.positionprediction.activities;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
@@ -29,7 +27,7 @@ import project.software.uni.positionprediction.util.Message;
 
 public class OSM extends AppCompatActivity {
 
-    OSMDroidMap mymap;
+    OSMDroidMap osmDroidMap;
 
     private Button buttonSettings = null;
     private Button buttonDownload = null;
@@ -45,7 +43,7 @@ public class OSM extends AppCompatActivity {
         // TODO: put this in own method
         // note: the actions in the OSMDroidMap constructor have to happen *before*
         // setContentView is called.
-        mymap = new OSMDroidMap(this);
+        osmDroidMap = new OSMDroidMap(this);
 
 
         setContentView(R.layout.activity_osm);
@@ -54,9 +52,9 @@ public class OSM extends AppCompatActivity {
         GeoPoint center = new GeoPoint(48.856359, 2.290849);
 
         try {
-            mymap.initMap(mapView, center, 6);
+            osmDroidMap.initMap(mapView, center, 6);
         } catch (MapInitException e) {
-            // todo: this will be moved to a controller anyway, handle errors there
+            // todo: this will (?) be moved to a controller anyway, handle errors there
             Message msg = new Message();
             msg.disp_error(this, "Error saving maps", "Could not save maps for offline use", true);
         }
@@ -97,7 +95,7 @@ public class OSM extends AppCompatActivity {
         // obtain an adapter
         OSMDroidVisualisationAdapter myVisAdap = new OSMDroidVisualisationAdapter();
         // set the map for the adapter
-        myVisAdap.linkMap(mymap);
+        myVisAdap.linkMap(osmDroidMap);
 
         // have it draw the visualisation
         // (I am making the assumption that an algorithm only has one specific fitting Visualisation)
@@ -156,7 +154,7 @@ public class OSM extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 BoundingBox subafrica = new BoundingBox(19.635663, 12.921289,7.006371,-4.305273);
-                mymap.saveAreaToCache(subafrica, 5,7);
+                osmDroidMap.saveAreaToCache(subafrica, 5,7);
             }
         });
 
@@ -179,11 +177,11 @@ public class OSM extends AppCompatActivity {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        if (mymap != null) mymap.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        if (osmDroidMap != null) osmDroidMap.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
     public void onPause(){
         super.onPause();
-        if (mymap != null) mymap.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        if (osmDroidMap != null) osmDroidMap.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 }
