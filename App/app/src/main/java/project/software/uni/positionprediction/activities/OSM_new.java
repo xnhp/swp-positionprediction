a4d574ec060c0aba2d51266c91ed96e012c06f88
Benjamin Moser <benjamin.2.moser@uni-konstanz.de>
2018-06-26 10:10:23 +0200

    reintroduce functionality to OSM activity

--- App/app/src/main/java/project/software/uni/positionprediction/activities/OSM_new.java
+++ App/app/src/main/java/project/software/uni/positionprediction/activities/OSM_new.java
@@ -2,12 +2,16 @@ package project.software.uni.positionprediction.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import org.osmdroid.config.Configuration;
 import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -35,11 +39,8 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
     // ========
 
 
-    OSMDroidMap mymap;
-
     private Button buttonSettings = null;
     private Button buttonDownload = null;
-    private Button buttonPanTo    = null;
     private Button buttonBack     = null;
     OSMDroidMap osmDroidMap;
 
@@ -58,16 +59,8 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
 
-
-        // 0.) Set Buttons
-        buttonSettings = findViewById(R.id.navbar_button_settings);
-        buttonBack = findViewById(R.id.navbar_button_back);
-        buttonDownload = findViewById(R.id.map_download_button);
-        //buttonPanTo    = findViewById(R.id.map_panto_button);
-        //registerEventHandlers(this);
 
 
         // 1.) Create OSMdroid map.
@@ -76,6 +69,7 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
         // This is the blank/default map, before sth. is drawn on it
         // TJ: todo: There should be an appropriate constructor in class "OSMdroidMap" to do this
         osmDroidMap = createMap(this, 2.290849, 48.856359, 6);
+
 
 
 
@@ -91,23 +85,20 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
         // 3.) Trigger prediction workflow
         // -------------------------------
 
-        // todo: this should be done in activity BirdSlelect or Settings
-
-        /*
-        PredictVisController_new pvContr = new PredictVisController_new(
-                this,
-                myVisAdap,
-                getPredictionUserParameters()
-        );
-        pvContr.trigger();
-*/
-
+        // todo: this should be done in activity BirdSelect or Settings
         PredictionWorkflow predWorkflow = new PredictionWorkflow(
                 this,
                 getPredictionUserParameters(),
                 myVisAdap
         );
         predWorkflow.trigger();
+
+        // register event handlers (button clicks etc)
+        // 0.) Set Buttons
+        buttonSettings = findViewById(R.id.navbar_button_settings);
+        buttonBack = findViewById(R.id.navbar_button_back);
+        buttonDownload = findViewById(R.id.map_download_button);
+        registerEventHandlers(this);
 
     }
 
@@ -121,8 +112,6 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
     // ==============================================================================
 
 
-
-
     // TJ: todo: Implement this Method correctly in class "Settings"
     public PredictionUserParameters getPredictionUserParameters() {
 
@@ -134,6 +123,7 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
         cl.add(Calendar.HOUR, hoursInFuture);
         Date date_pred = cl.getTime();
 
+        // todo: fetch from PredictionWorkflow controller instead
         Intent i = getIntent();
         Bird bird = (Bird) i.getSerializableExtra("selectedBird");
 
@@ -153,42 +143,49 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
     private OSMDroidMap createMap(Context ctx, double centerLon, double centerLat, int zoom){
         // note: the actions in the OSMDroidMap constructor have to happen *before*
         // setContentView is called.
-        mymap = new OSMDroidMap(ctx);
+        osmDroidMap = new OSMDroidMap(ctx);
 
         setContentView(R.layout.activity_osm);
 
         MapView mapView = (MapView) findViewById(R.id.map);
         GeoPoint center = new GeoPoint(centerLat, centerLon);
         try {
-            mymap.initMap(mapView, center, zoom);
+            osmDroidMap.initMap(mapView, center, zoom);
         } catch(MapInitException e) {
             Log.i("OSM_new", e.getMessage());
         }
-        return mymap;
+        return osmDroidMap;
     }
 
-    // TJ 180612: Copied from Benny
 
+    /*
+     * On activity resume
+     */
     public void onResume(){
         super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        if (mymap != null) mymap.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        // this will refresh the osmdroid configuration on resuming.
+        // if you make changes to the configuration, use
+        // SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        if (osmDroidMap != null) osmDroidMap.onResume(); // needed for overlays (location, ...)
     }
 
+    /*
+     * On activity pause
+     */
     public void onPause(){
-        //fo
         super.onPause();
-        if (mymap != null) mymap.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        if (osmDroidMap != null) osmDroidMap.onPause();  // needed for overlays (location, ...)
     }
 
-    private void registerEventHandlers(final OSM_new osm) {
+    /*
+     * Register event handlers for user interaction (buttons etc)
+     */
+    private void registerEventHandlers(final Context ctx) {
         buttonSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent buttonIntent =  new Intent(osm, Settings.class);
+                Intent buttonIntent =  new Intent(ctx, Settings.class);
                 startActivity(buttonIntent);
             }
         });
@@ -200,21 +197,35 @@ public class OSM_new extends AppCompatActivity implements FloatingMapButtons.flo
             }
         });
 
-
         buttonDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 BoundingBox subafrica = new BoundingBox(19.635663, 12.921289,7.006371,-4.305273);
-                mymap.saveAreaToCache(subafrica, 5,7);
+                osmDroidMap.saveAreaToCache(subafrica, 5,7);
+            }
+        });
+
+        /* manual interaction with the map will always disable osmdroid's
+           followLocation. This is used to update the button accordingly.
+        */
+        osmDroidMap.mapView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.i("foo", "on touch listener on mapview called, returning true");
+                toggleShowLocBtn(false);
+                return false; // indicates that the touch event was not
+                // successfully handled (it will propagated
+                // further, such as to the mapView to actually
+                // pan/zoom/rotate it).
             }
         });
     }
 
 
-/**
- * Click handler triggered by the according button in
- * fragments.FloatingMapButtons
- */
+    /**
+     * Click handler triggered by the according button in
+     * fragments.FloatingMapButtons
+     */
     @Override
     public void onSwitchModeClick() {
         // switch to Cesium Activity
