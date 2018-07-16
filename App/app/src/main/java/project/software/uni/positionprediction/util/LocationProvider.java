fdf79e30d4f3cab72e642930eee4545cfd892f1f
Benjamin Moser <benjamin.2.moser@uni-konstanz.de>
2018-07-16 17:24:36 +0200

    null check for location provider

--- App/app/src/main/java/project/software/uni/positionprediction/util/LocationProvider.java
+++ App/app/src/main/java/project/software/uni/positionprediction/util/LocationProvider.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -33,9 +32,14 @@ public class LocationProvider {
 
         // because receiving the first location update might take a little bit,
         // we call onLocationChanged immediately with the last known location.
-        listener.onLocationChanged(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        // this probably makes some later null checks obsolete
+        if (lastLocation != null) {
+            listener.onLocationChanged(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+        }
 
         // attach listener
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, listener);
+
     }
 }
